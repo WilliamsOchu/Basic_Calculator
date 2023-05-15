4cdca3404c581bbbe32da92e8d0fcf7b7f1c0c56
unplugged001 <ochuwilliamsirawo@gmail.com>
2023-05-15 13:26:12 +0100

    Modified code to ensure a user does not input an alphabet at runtime

--- Basic_Calc.py
+++ Basic_Calc.py
@@ -2,36 +2,52 @@
 ## It will ask a user to input a first digit, input an operator and then input a second digit
 
 
-nums1=float(input("Enter the Digit: "))
-print("\n")
-operator=input("Input your operator: ")
-print("\n")
-nums2=float(input("Enter second digit: "))
-print("\n")
+## Code modified to prevent a user from getting an error when he enters an alphabet instead of a number 
+
 
 def calcops():
-    match operator:
-        case "+":
-            result=nums1+nums2
-        case "-":
-            result=nums1-nums2
-        case "*":
-            result=nums1*nums2
-        case "/":
-            result=nums1/nums2
-        case "//":
-            result=nums1//nums2
-        case "%":
-            result=nums1%nums2
-        case "**":
-            result=nums1**nums2
-        case _:
-            result="You have entered an incorrect operand"
-    return result    
+    while True:
+        try:
+            
+            nums1=float(input("Enter the first Digit: "))
+            print("\n")
+            operator=input("Input your operator: ")
+            print("\n")
+            nums2=float(input("Enter second digit: "))
+            print("\n")
+    
+
+            match operator:
+                case "+":
+                    result=nums1+nums2
+                case "-":
+                    result=nums1-nums2
+                case "*":
+                    result=nums1*nums2
+                case "/":
+                    result=nums1/nums2
+                case "//":
+                    result=nums1//nums2
+                case "%":
+                    result=nums1%nums2
+                case "**":
+                    result=nums1**nums2
+                case _:
+                    result="You have entered an incorrect operand"
+                    
+        except ValueError:
+            print("Enter numbers only")
+            
+            
+        else:
+            break
+                        
+    return result
+    
 
 
 output=calcops()
-print("The result of this maths operation is: ", output) 
+print("The result of this maths operation is: ",output) 
 print("\n")   
     
         
